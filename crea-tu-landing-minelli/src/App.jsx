5f42f80bf9bfc4a4c2f17f4267055b8ca6a124ef
Santiago-Minelli <santiagominelli98@gmail.com>
2025-05-12 11:41:11 -0300

    TST Cards & Estilos

--- crea-tu-landing-minelli/src/App.jsx
+++ crea-tu-landing-minelli/src/App.jsx
@@ -1,5 +1,6 @@
-import NavBar from "./components/NavBar/NavBar"
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 function App() {
 
--- crea-tu-landing-minelli/src/components/ItemListContainer/ItemListContainer.jsx
+++ crea-tu-landing-minelli/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,47 @@
+import "./ItemListContainer.css";
 
 function ItemListContainer({message}) {
     return (
         <div>
-            <h1>{message}</h1>
+            <div className="encabezado">
+                <h1>{message}</h1>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-3">
+                        <div className="card">
+                            <img className="imagen-card" alt="TST Imagen" src="/logo-buenas-cosas.png"/>
+                            <h2>TST Pastelería 1</h2>
+                            <p>$$$$$$</p>
+                            <button>Añadir</button>
+                        </div>                       
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <img className="imagen-card" alt="TST Imagen" src="/logo-buenas-cosas.png"/>
+                            <h2>TST Pastelería 2</h2>
+                            <p>$$$$$$</p>
+                            <button>Añadir</button>
+                        </div>   
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <img className="imagen-card" alt="TST Imagen" src="/logo-buenas-cosas.png"/>
+                            <h2>TST Pastelería 3</h2>
+                            <p>$$$$$$</p>
+                            <button>Añadir</button>
+                        </div>   
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <img className="imagen-card" alt="TST Imagen" src="/logo-buenas-cosas.png"/>
+                            <h2>TST Pastelería 4</h2>
+                            <p>$$$$$$</p>
+                            <button>Añadir</button>
+                        </div>   
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
