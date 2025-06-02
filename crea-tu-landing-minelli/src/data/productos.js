49a9469f72dc0a4a85bf49bbe1debf6da9a32454
Santiago-Minelli <santiagominelli98@gmail.com>
2025-06-02 19:50:43 -0300

    BD

--- crea-tu-landing-minelli/src/data/productos.js
+++ crea-tu-landing-minelli/src/data/productos.js
@@ -1,27 +1,76 @@
 const productosBuenasCosas = [
     {
         id: 1,
-        nombre: "s",
-        descripcion: "s",
-        precio: 12.99,
-        imagen: "../../public/logo-buenas-cosas.png",
+        nombre: "Budín de Limon",
+        descripcion: "Un exquisito budín de Limón bañado con una fina capa de glaseado",
+        precio: 2499,
+        imagen: "../../public/budin-limon.png",
         categoria: "Pasteleria"
     },
     {
         id: 2,
-        nombre: "a",
-        descripcion: "asa",
-        precio: 12.50,
-        imagen: "../../public/logo-buenas-cosas.png",
+        nombre: "Pan Lactal",
+        descripcion: "Pan Lactal ideal para sandwiches, ¡Y ES SIN TACC!",
+        precio: 8000,
+        imagen: "../../public/pan-lactal.png",
         categoria: "Panificacion"
     },
         {
         id: 3,
-        nombre: "n",
-        descripcion: "annnsa",
-        precio: 18.50,
-        imagen: "../../public/logo-buenas-cosas.png",
+        nombre: "Marquees de Naranja",
+        descripcion: "Nada como una Marquees de Naranja de postre para cerrar esos lindos momentos",
+        precio: 8850,
+        imagen: "../../public/marquees-naranja.png",
         categoria: "Pasteleria"
+    },
+        {
+        id: 4,
+        nombre: "Cara Sucia",
+        descripcion: "Unas ricas Cara Sucia para desayunar o merendar",
+        precio: 2000,
+        imagen: "../../public/cara-sucia.png",
+        categoria: "Panificacion"
+    },
+        {
+        id: 5,
+        nombre: "Pan de Campo",
+        descripcion: "El Pan de Campo, ideal para acompañar con todo",
+        precio: 6200,
+        imagen: "../../public/pan-campo.png",
+        categoria: "Panificacion"
+    },
+        {
+        id: 6,
+        nombre: "Pan Árabe",
+        descripcion: "Solo el que come de estos panes sabe lo rico que son",
+        precio: 4750,
+        imagen: "../../public/pan-arabe.png",
+        categoria: "Panificacion"
+    },
+        {
+        id: 7,
+        nombre: "Selva Negra",
+        descripcion: "El postre perfecto para los adictos al chocolate",
+        precio: 8500,
+        imagen: "../../public/pan-arabe.png",
+        categoria: "Pasteleria"
+    },
+        {
+        id: 8,
+        nombre: "Mil Hojas",
+        descripcion: "El Mil Hojas, postre que pareciera que no terminara nunca",
+        precio: 7800,
+        imagen: "../../public/mil-hojas.png",
+        categoria: "Pasteleria"
+    }
+    ,
+        {
+        id: 8,
+        nombre: "Medias Lunas",
+        descripcion: "Nada como unas Medias Lunas junto a un mate o café",
+        precio: 7800,
+        imagen: "../../public/media-luna.png",
+        categoria: "Panificacion"
     }
 ];
 
